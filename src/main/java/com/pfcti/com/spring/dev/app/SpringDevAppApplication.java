854fdb0a2af1586797339cae11248f3cd5dc88f4
Luis Quiros <luis.quiros@pfcti.com>
2023-04-18 19:32:08 -0600

    Actualización Práctica

--- src/main/java/com/pfcti/com/spring/dev/app/SpringDevAppApplication.java
+++ src/main/java/com/pfcti/com/spring/dev/app/SpringDevAppApplication.java
@@ -3,6 +3,7 @@ package com.pfcti.com.spring.dev.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringDevAppApplication {
 
