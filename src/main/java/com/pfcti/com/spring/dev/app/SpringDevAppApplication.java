c1bb9a9b66a4764ad71dccfa2c4a80842020ae87
Luis Quiros <luis.quiros@pfcti.com>
2023-04-18 18:07:36 -0600

    Primera Práctica

---
+++ src/main/java/com/pfcti/com/spring/dev/app/SpringDevAppApplication.java
@@ -0,0 +1,13 @@
+package com.pfcti.com.spring.dev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfcti/com/spring/dev/app/SpringDevAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfcti.com.spring.dev.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
